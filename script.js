7cd9933ee1798aec351bf5493763dbfe9f56a6a2
JesusA0416 <0119lopez@gmail.com>
2025-05-15 13:01:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -40,77 +40,102 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(card);
   });
 });
-const c = document.getElementById('heartCanvas');
-const a = c.getContext('2d');
-let e = [], h = [];
-let O = c.width;
-let Q = c.height;
+const canvas = document.getElementById('heartCanvas');
+if (canvas) {
+  const ctx = canvas.getContext('2d');
+  let O = canvas.width;
+  let Q = canvas.height;
+  const Y = 6.3;
+  const M = Math;
+  const R = M.random;
+  const C = M.cos;
+  const v = 32;
+  let h = [];
+  let e = [];
 
-let v = 32, M = Math, R = M.random, C = M.cos, Y = 6.3;
-for (let i = 0; i < Y; i += .2) {
-  h.push([
-    O / 2 + 180 * M.pow(M.sin(i), 3),
-    Q / 2 + 10 * (-(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)))
-  ]);
-}
-
-for (let i = 0; i < v; i++) {
-  let f = [], x = R() * O, y = R() * Q, H = i / v * 80 + 280, S = R() * 40 + 60, B = R() * 60 + 20;
-  for (let k = 0; k < v; k++) {
-    f[k] = {
-      x: x, y: y, X: 0, Y: 0,
-      R: (1 - k / v) * 4 + 2,
-      S: R() + 1,
-      q: ~~(R() * v),
-      D: i % 2 * 2 - 1,
-      F: R() * .2 + .7,
-      f: `hsla(${~~H},${~~S}%,${~~B}%,.1)`
-    };
+  for (let i = 0; i < Y; i += .2) {
+    h.push([
+      O / 2 + 180 * M.pow(M.sin(i), 3),
+      Q / 2 + 10 * (-(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)))
+    ]);
   }
-  e[i] = f;
-}
 
-function render(p) {
-  a.fillStyle = p.f;
-  a.beginPath();
-  a.arc(p.x, p.y, p.R, 0, Y, 1);
-  a.closePath();
-  a.fill();
-}
+  for (let i = 0; i < v; i++) {
+    let x = R() * O;
+    let y = R() * Q;
+    let H = i / v * 80 + 280;
+    let S = R() * 40 + 60;
+    let B = R() * 60 + 20;
+    let f = [];
 
-function loop() {
-  a.fillStyle = "rgba(0,0,0,.2)";
-  a.fillRect(0, 0, O, Q);
+    for (let k = 0; k < v; k++) {
+      f[k] = {
+        x: x,
+        y: y,
+        X: 0,
+        Y: 0,
+        R: (1 - k / v) + 1,
+        S: R() + 1,
+        q: ~~(R() * v),
+        D: i % 2 * 2 - 1,
+        F: R() * .2 + .7,
+        f: "hsla(" + ~~H + "," + ~~S + "%," + ~~B + "%,.1)"
+      };
+    }
 
-  for (let i = v; i--;) {
-    let f = e[i], u = f[0], q = h[u.q], D = u.x - q[0], E = u.y - q[1];
-    let G = M.sqrt(D * D + E * E);
+    e[i] = f;
+  }
 
-    if (G < 10) {
-      if (R() > .95) u.q = ~~(R() * v);
-      else {
-        if (R() > .99) u.D *= -1;
-        u.q = (u.q + u.D + v) % v;
+  function render(_) {
+    ctx.fillStyle = _.f;
+    ctx.beginPath();
+    ctx.arc(_.x, _.y, _.R, 0, Y, 1);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function loop() {
+    ctx.fillStyle = "rgba(0,0,0,.2)";
+    ctx.fillRect(0, 0, O, Q);
+
+    for (let i = 0; i < v; i++) {
+      let f = e[i];
+      let u = f[0];
+      let q = h[u.q];
+      let D = u.x - q[0];
+      let E = u.y - q[1];
+      let G = M.sqrt(D * D + E * E);
+
+      if (G < 10) {
+        if (R() > .95) {
+          u.q = ~~(R() * v);
+        } else {
+          if (R() > .99) u.D *= -1;
+          u.q += u.D;
+          u.q %= v;
+          if (u.q < 0) u.q += v;
+        }
+      }
+
+      u.X += -D / G * u.S;
+      u.Y += -E / G * u.S;
+      u.x += u.X;
+      u.y += u.Y;
+      render(u);
+      u.X *= u.F;
+      u.Y *= u.F;
+
+      for (let k = 0; k < v - 1; k++) {
+        let T = f[k];
+        let N = f[k + 1];
+        N.x -= (N.x - T.x) * .7;
+        N.y -= (N.y - T.y) * .7;
+        render(N);
       }
     }
 
-    u.X += -D / G * u.S;
-    u.Y += -E / G * u.S;
-    u.x += u.X;
-    u.y += u.Y;
-    u.X *= u.F;
-    u.Y *= u.F;
-
-    render(u);
-    for (let k = 0; k < v - 1; k++) {
-      let T = f[k], N = f[k + 1];
-      N.x -= (N.x - T.x) * .7;
-      N.y -= (N.y - T.y) * .7;
-      render(N);
-    }
+    requestAnimationFrame(loop);
   }
 
-  requestAnimationFrame(loop);
+  loop();
 }
-
-loop();
