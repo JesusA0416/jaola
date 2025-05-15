080bb169fd6c5236af032c41be41bf1aeb54e0d9
JesusA0416 <0119lopez@gmail.com>
2025-05-15 12:04:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,77 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(card);
   });
 });
+const c = document.getElementById('heartCanvas');
+const a = c.getContext('2d');
+let e = [], h = [];
+let O = c.width = innerWidth;
+let Q = c.height = 400;
+
+let v = 32, M = Math, R = M.random, C = M.cos, Y = 6.3;
+for (let i = 0; i < Y; i += .2) {
+  h.push([
+    O / 2 + 180 * M.pow(M.sin(i), 3),
+    Q / 2 + 10 * (-(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)))
+  ]);
+}
+
+for (let i = 0; i < v; i++) {
+  let f = [], x = R() * O, y = R() * Q, H = i / v * 80 + 280, S = R() * 40 + 60, B = R() * 60 + 20;
+  for (let k = 0; k < v; k++) {
+    f[k] = {
+      x: x, y: y, X: 0, Y: 0,
+      R: (1 - k / v) + 1,
+      S: R() + 1,
+      q: ~~(R() * v),
+      D: i % 2 * 2 - 1,
+      F: R() * .2 + .7,
+      f: `hsla(${~~H},${~~S}%,${~~B}%,.1)`
+    };
+  }
+  e[i] = f;
+}
+
+function render(p) {
+  a.fillStyle = p.f;
+  a.beginPath();
+  a.arc(p.x, p.y, p.R, 0, Y, 1);
+  a.closePath();
+  a.fill();
+}
+
+function loop() {
+  a.fillStyle = "rgba(0,0,0,.2)";
+  a.fillRect(0, 0, O, Q);
+
+  for (let i = v; i--;) {
+    let f = e[i], u = f[0], q = h[u.q], D = u.x - q[0], E = u.y - q[1];
+    let G = M.sqrt(D * D + E * E);
+
+    if (G < 10) {
+      if (R() > .95) u.q = ~~(R() * v);
+      else {
+        if (R() > .99) u.D *= -1;
+        u.q = (u.q + u.D + v) % v;
+      }
+    }
+
+    u.X += -D / G * u.S;
+    u.Y += -E / G * u.S;
+    u.x += u.X;
+    u.y += u.Y;
+    u.X *= u.F;
+    u.Y *= u.F;
+
+    render(u);
+    for (let k = 0; k < v - 1; k++) {
+      let T = f[k], N = f[k + 1];
+      N.x -= (N.x - T.x) * .7;
+      N.y -= (N.y - T.y) * .7;
+      render(N);
+    }
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
