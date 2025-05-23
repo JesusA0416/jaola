56ebe59f52a1287d0b9b708c8f99201d9e1271ff
JesusA0416 <0119lopez@gmail.com>
2025-05-23 12:10:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,25 @@ function initHeartCanvas() {
 document.addEventListener('DOMContentLoaded', () => {
   initHeartCanvas();
 });
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          elmnt.innerHTML = this.responseText;
+          includeHTML(); // run again for nested includes
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+
+includeHTML(); // call it when the page loads
